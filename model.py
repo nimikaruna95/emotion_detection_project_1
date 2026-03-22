496828c579997f892297196430ec5c9120b381ea
nimikaruna95 <165786611+nimikaruna95@users.noreply.github.com>
2026-03-22 21:26:17 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,28 +1,42 @@
-#model.py
-import torch
+# model.py
+
 import torch.nn as nn
 import torch.nn.functional as F
 
 class EmotionCNN(nn.Module):
-    def __init__(self, num_classes=7):
-        super(EmotionCNN, self).__init__()
-        
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
-        
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-        
-        self.fc1 = nn.Linear(128 * 6 * 6, 512)
-        self.fc2 = nn.Linear(512, num_classes)
 
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x)))
-        
-        x = x.view(-1, 128 * 6 * 6)  # Flatten
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        
-        return x
+    def __init__(self, num_classes=7):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(1,32,3,padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+
+        self.conv2 = nn.Conv2d(32,64,3,padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+
+        self.conv3 = nn.Conv2d(64,128,3,padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+
+        self.conv4 = nn.Conv2d(128,256,3,padding=1)
+        self.bn4 = nn.BatchNorm2d(256)
+
+        self.pool = nn.MaxPool2d(2)
+
+        self.dropout = nn.Dropout(0.5)
+
+        self.fc1 = nn.Linear(256*3*3,512)
+        self.fc2 = nn.Linear(512,num_classes)
+
+    def forward(self,x):
+
+        x=self.pool(F.relu(self.bn1(self.conv1(x))))
+        x=self.pool(F.relu(self.bn2(self.conv2(x))))
+        x=self.pool(F.relu(self.bn3(self.conv3(x))))
+        x=self.pool(F.relu(self.bn4(self.conv4(x))))
+
+        x=x.view(x.size(0),-1)
+
+        x=self.dropout(F.relu(self.fc1(x)))
+
+        return self.fc2(x)
+
