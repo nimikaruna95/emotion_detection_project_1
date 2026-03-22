99ad0f028e0474e96edb15ef8edd35bed2e7b776
nimikaruna95 <165786611+nimikaruna95@users.noreply.github.com>
2026-03-22 21:19:50 +0530

    Update dataset_loader.py

--- dataset_loader.py
+++ dataset_loader.py
@@ -1,18 +1,18 @@
 #dataset_loader.py
-import torch
 from torchvision import transforms
 from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
 
 def load_fer2013(batch_size=32):
     transform = transforms.Compose([
-        transforms.Grayscale(num_output_channels=1),  # Convert to grayscale (FER-2013 is grayscale)
-        transforms.Resize((48, 48)),  # Resize images to 48x48
-        transforms.ToTensor(),  # Convert image to tensor
-        transforms.Normalize(mean=[0.5], std=[0.5])  # Normalize pixel values
+        transforms.Grayscale(num_output_channels=1),  
+        transforms.Resize((48, 48)),  
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(10),
+        transforms.ToTensor(),  
+        transforms.Normalize(mean=[0.5], std=[0.5])  
     ])
 
-    # Load dataset using ImageFolder
     train_set = ImageFolder(root="data/fer2013_data/train", transform=transform)
     test_set = ImageFolder(root="data/fer2013_data/test", transform=transform)
 
@@ -21,9 +21,9 @@ def load_fer2013(batch_size=32):
 
     return train_loader, test_loader
 
-# Run this file to check if dataset loads correctly
 if __name__ == "__main__":
     train_loader, test_loader = load_fer2013()
     print(f"Train dataset size: {len(train_loader.dataset)}")
     print(f"Test dataset size: {len(test_loader.dataset)}")
+
 
